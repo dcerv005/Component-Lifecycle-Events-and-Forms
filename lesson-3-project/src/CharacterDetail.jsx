e98bc0c918e1a8c17220d01d1ee8a25523d12543
dcerv005 <163106687+dcerv005@users.noreply.github.com>
2024-07-16 15:41:08 -0700

    Update CharacterDetail.jsx

--- lesson-3-project/src/CharacterDetail.jsx
+++ lesson-3-project/src/CharacterDetail.jsx
@@ -5,24 +5,26 @@ import {number} from "prop-types"
 
 const CharacterDetails = (props) => {
     const [id] = useState(props.characterId)
-    const [character, setCharacter]= useState([])
-    const ts= new Date().getTime()
+    const [character, setCharacter]= useState(null)    
     
     
     useEffect( () => {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`https://gateway.marvel.com/v1/public/characters/${id}?&ts=1&apikey=1db16fa474c3fdf9e74d956d788ea55f&hash=e1c499a5ceb1d8ab8e4afbeb45348103`);
-                setCharacter(response.data.data.results[0])
                 console.log(response.data.data.results[0])
-                console.log(character)
+                setCharacter(response.data.data.results[0])
                 
             } catch (error) {
                 console.error('Error fetching products', error)
             }
-        }
+        };
         fetchData();
-    }, []);
+    }, [id]);
+
+    if (!character) {
+        return <div> Loading... </div>
+    }
 
     
    
@@ -31,7 +33,6 @@ const CharacterDetails = (props) => {
         <div>
             <h1>{character.name}</h1>
             <p>{character.description}</p>
-            {/* <p>{character.comics.available}</p> */}
             <ul>
                 {character.comics.items.map((comic, index)=> (
                     <li key={index}>{comic.name}</li>
@@ -42,8 +43,8 @@ const CharacterDetails = (props) => {
     )
 }
 
-CharacterDetails.propType = {
-    characterId: number
+CharacterDetails.propTypes = {
+    characterId: number.isRequired
 }
 
 export default CharacterDetails;
